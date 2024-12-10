57f929a0e6590cf0ed60567597929ab461e09670
Boburmirzo <boburforfun@gmail.com>
2024-12-10 13:41:05 +0500

    refactoring

--- api-gateway/api.go
+++ api-gateway/api.go
@@ -6,71 +6,72 @@ import (
 	"os"
 )
 
-var(
-    ORDERS_SERVICE = os.Getenv("orders")
-    USERS_SERVICE = os.Getenv("users")
-    INVENTORY_SERVICE = os.Getenv("inventory")
+var (
+	ORDERS_SERVICE    = os.Getenv("orders")
+	USERS_SERVICE     = os.Getenv("users")
+	INVENTORY_SERVICE = os.Getenv("inventory")
 )
 
-type API struct{
-    middleware []Middleware
+type API struct {
+	middleware []Middleware
 }
 
-func NewAPI() *API{
-    return &API{}
+func NewAPI() *API {
+	return &API{}
 }
 
-func forwardHeaders(resp *http.Response, w http.ResponseWriter){
-    for key, values := range resp.Header{
-        for _, v := range values {
-            w.Header().Add(key, v)
-        }
-    }
+func forwardHeaders(resp *http.Response, w http.ResponseWriter) {
+	for key, values := range resp.Header {
+		for _, v := range values {
+			w.Header().Add(key, v)
+		}
+	}
 }
 
-func (a *API) handleRequests(w http.ResponseWriter, r *http.Request){
+func (a *API) handleRequests(w http.ResponseWriter, r *http.Request) {
 
-    services := map[string]string{
-        "/orders":ORDERS_SERVICE,
-        "/products":INVENTORY_SERVICE,
-        "/users":USERS_SERVICE,
-    }
+	services := map[string]string{
+		"/orders":   ORDERS_SERVICE,
+		"/products": INVENTORY_SERVICE,
+		"/users":    USERS_SERVICE,
+	}
 
-    service, ok := services[r.URL.Path]
+	service, ok := services[r.URL.Path]
 
-    if !ok{
-        http.Error(w, "service not found", http.StatusNotFound)
-        return
-    }
+	if !ok {
+		http.Error(w, "service not found", http.StatusNotFound)
+		return
+	}
 
-    req, err := http.NewRequest(r.Method, service + r.URL.Path, r.Body)
-    defer r.Body.Close()
-    if err != nil{
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return 
-    }
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil{
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    forwardHeaders(resp, w)
-    io.Copy(w, resp.Body)
-    w.WriteHeader(resp.StatusCode)
+	req, err := http.NewRequest(r.Method, service+r.URL.Path, r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	forwardHeaders(resp, w)
+	io.Copy(w, resp.Body)
+	w.WriteHeader(resp.StatusCode)
 }
 
 func (a *API) use(m Middleware) {
-    a.middleware = append(a.middleware, m)
+	a.middleware = append(a.middleware, m)
 }
 
 func (a *API) mount() http.Handler {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", a.handleRequests)
-    //middleware happens here
-    handler := middlewareFunc(mux, ) 
-    return  handler 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", a.handleRequests)
+	//middleware happens here
+	a.use(JWTValidate)
+	handler := middlewareFunc(mux)
+	return handler
 }
 
 func (a *API) run(addr string) error {
-    return http.ListenAndServe(addr, a.mount())
+	return http.ListenAndServe(addr, a.mount())
 }
--- api-gateway/middleware.go
+++ api-gateway/middleware.go
@@ -4,68 +4,65 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
-type MiddlewareError struct{
-    err string 
-    status int
+type userKey string
+
+const user_id_key userKey = "user_id"
+
+type MiddlewareError struct {
+	err    string
+	status int
 }
 
-func NewMiddlewareError(err string, s int) *MiddlewareError{
-    return &MiddlewareError{
-        err: err,
-        status: s,
-    }
+func NewMiddlewareError(err string, s int) *MiddlewareError {
+	return &MiddlewareError{
+		err:    err,
+		status: s,
+	}
 }
 
-func (m *MiddlewareError) setError(msg string) {
-    m.err = msg
-}
+type Middleware func(w http.ResponseWriter, r **http.Request) *MiddlewareError
 
-func (m *MiddlewareError) setStatus(s int){
-    m.status = s
-}
-
-type Middleware func(w http.ResponseWriter, r *http.Request) *MiddlewareError
-
-
-func middlewareFunc(next http.Handler, middlwareFuncs...Middleware)  http.Handler {
+func middlewareFunc(next http.Handler, middlwareFuncs ...Middleware) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        start := time.Now()
+		start := time.Now()
 
-        defer func(){
-            log.Printf("Took: %f", time.Since(start).Seconds())
-        }()
-        for _, m := range middlwareFuncs{
-            // stop the chain if one of middleware functions return an error
-            if resp := m(w, r); resp.err != ""{
-                http.Error(w, resp.err, resp.status)
-                return 
-            }
-        }
+		defer func() {
+			log.Printf("Took: %f", time.Since(start).Seconds())
+		}()
+		for _, m := range middlwareFuncs {
+			// stop the chain if one of middleware functions return an error
+			if resp := m(w, &r); resp.err != "" {
+				http.Error(w, resp.err, resp.status)
+				return
+			}
+		}
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func JWTValidate(w http.ResponseWriter, r *http.Request) *MiddlewareError {
+func JWTValidate(w http.ResponseWriter, req **http.Request) *MiddlewareError {
+	r := *req
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return NewMiddlewareError("unauthorized", http.StatusUnauthorized)
+	}
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return NewMiddlewareError("unauthorized", http.StatusUnauthorized)
+	}
 
-    authHeader := r.Header.Get("Authorization")
-    if authHeader == "" {
-        return NewMiddlewareError("unauthorized", http.StatusUnauthorized) 
-    }
-    parts := strings.Split(authHeader, " ")
-    if len(parts) != 2 || parts[0] != "Bearer" {
-        return NewMiddlewareError("unauthorized", http.StatusUnauthorized) 
-    }
-
-    token := parts[1]
-    claims, err := ValidateToken(token)
-    if err != nil {
-        return NewMiddlewareError("unauthorized", http.StatusUnauthorized) 
-    }
-    ctx := context.WithValue(r.Context(), "user_id", claims.UserID) 
-    r.WithContext(ctx)
-    return nil 
+	token := parts[1]
+	claims, err := ValidateToken(token)
+	if err != nil {
+		return NewMiddlewareError("unauthorized", http.StatusUnauthorized)
+	}
+	ctx := context.WithValue(r.Context(), user_id_key, claims.UserID)
+	withCtxReq := r.WithContext(ctx)
+	*r = *withCtxReq
+	return nil
 }
